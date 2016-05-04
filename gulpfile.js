9beac69c627e2db0663eeea3c164060585312a4a
Khalid Adisendjaja <kh411d@yahoo.com>
2016-05-04 12:16:24 +0700

    optimize pagespeed

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,18 @@ var minifyCss     = require('gulp-minify-css');
 const cssFiles = ['public/css/poole.css','public/css/syntax.css','public/css/hyde.css'];
 var imagemin    = require('gulp-imagemin');
 var pngquant    = require('imagemin-pngquant');
+var jpegtran = require('imagemin-jpegtran');
+var gifsicle = require('imagemin-gifsicle');
 
-gulp.task('css', () => {
+/*gulp.task('css', () => {
   gulp.src(cssFiles)
     .pipe(concat('all.css'))
     .pipe(minifyCss())
     .pipe(gulp.dest('public/css'))
 
-});
+});*/
 
-gulp.task('imagemin',() => {
+/*gulp.task('imagemin',() => {
 	gulp.src(['images/*'])
      .pipe(imagemin({
        progressive: true,
@@ -22,12 +24,21 @@ gulp.task('imagemin',() => {
        })]
      }))
      .pipe(gulp.dest('_site/images/'));
+});*/
+
+gulp.task('optimize-images', function () {
+    return gulp.src(['images/**/*.jpg', 'images/**/*.jpeg', 'images/**/*.gif', 'images/**/*.png'])
+        .pipe(imagemin({
+            progressive: false,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant(), jpegtran(), gifsicle()]
+        }))
+        .pipe(gulp.dest('optimized-images/'));
 });
 
 
-
-gulp.task('watch', () => {
+/*gulp.task('watch', () => {
   gulp.watch(cssFiles, ['css']);
-});
+});*/
 
-gulp.task('default', ['css','imagemin', 'watch']);
+gulp.task('default', ['optimize-images']);
